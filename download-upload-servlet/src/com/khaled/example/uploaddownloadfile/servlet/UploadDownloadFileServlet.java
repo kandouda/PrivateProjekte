e048ac8665c4de46cec1181c6d2ec4a959e79624
Khaled El Zein <khaled.elzein@prodyna.com>
2014-04-10 01:24:30 +0200

    upload and download File servlet

---
+++ download-upload-servlet/src/com/khaled/example/uploaddownloadfile/servlet/UploadDownloadFileServlet.java
@@ -0,0 +1,57 @@
+package com.khaled.example.uploaddownloadfile.servlet;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
+/**
+ * Servlet implementation class UploadDownloadFileServlet
+ */
+@WebServlet("/updownload.do")
+public class UploadDownloadFileServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	private ServletFileUpload uploader = null;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UploadDownloadFileServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init() throws ServletException {
+		DiskFileItemFactory fileFactory = new DiskFileItemFactory();
+		File filesDir = (File) getServletContext().getAttribute("FILES_DIR_FILE");
+		fileFactory.setRepository(filesDir);
+		this.uploader = new ServletFileUpload(fileFactory);
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("doGet Called");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("doPost Called");
+	}
+
+}
